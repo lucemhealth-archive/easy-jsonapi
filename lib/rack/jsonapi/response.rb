0b7bcd48d10ea55037ddb5c5fe6400554fe68f6f
Joshua DeMoss <joshua.demoss@curatess.com>
2020-12-23 23:09:26 -0600

    added a validate method that includes validate_body and validate_headers. also added tests for it.

--- lib/rack/jsonapi/response.rb
+++ lib/rack/jsonapi/response.rb
@@ -8,12 +8,22 @@ module JSONAPI
 
     # @param body [Hash] The ruby hash mimicking jsonapi to check for compliance
     # @return [Nilclass] if no errors are found
+    # @raise [InvalidDocument | InvalidHeader] depending on what errors were found
+    def self.validate(body, headers)
+      validate_headers(headers)
+      validate_body(body)
+    end
+    
+    # @param (see #validate)
+    # @return (see #validate)
     # @raise InvalidDocument if document found to be non compliant
     def self.validate_body(body)
       JSONAPI::Exceptions::DocumentExceptions.check_compliance(body, is_a_request: false)
     end
-
-    # @param headers [Hash] The headers hash to be returned in the rack response
+    
+    # @param (see #validate)
+    # @return (see #validate)
+    # @raise InvalidHeader if header found to be non compliant
     def self.validate_headers(headers)
       JSONAPI::Exceptions::HeadersExceptions.check_compliance(headers.transform_keys(&:upcase))
     end
